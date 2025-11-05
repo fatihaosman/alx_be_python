20fa92966ab23428a496d32c64d1eb11180caf84
fatihaosman <fatihaosman17@gmail.com>
2025-11-05 19:28:27 +0300

    made corrects on hours tos ec

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,4 +1,4 @@
 hours = 2 # number of hours
-seconds_in_an_hour = 3600 # number of seconds in one hour
-seconds= hours * seconds_in_an_hour
+seconds = 3600 # number of seconds in one hour
+seconds= hours * seconds
 print( hours, "hour(s) is:", seconds ,"seconds")
