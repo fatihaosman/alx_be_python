438a204fb9606077ca3f03aa810475120d26fd87
fatihaosman <fatihaosman17@gmail.com>
2025-11-05 19:14:13 +0300

    just did simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,6 @@
+principal = 1000 # initial amount of money
+rate = 0.05       # interest rate in percentage
+time = 3         # time in years
+# calculate simple interest
+simple_interest = principal * rate * time
+print("The simple interest is:", simple_interest)
