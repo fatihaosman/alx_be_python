71192aab5ca31c45679266e6ec2b8aad4b137cb3
fatihaosman <fatihaosman17@gmail.com>
2025-11-05 18:30:24 +0300

    first commit - just added my first python code

---
+++ basic_operations.py
@@ -0,0 +1,25 @@
+# student_count = 1000  
+# rating = 4.99
+# is_published = True
+# Course_name = "pythoon programming"
+# print(student_count) = False 
+
+
+# name = input("What is your name? ")
+# print("Hello,", name)  # Personalized greeting using the input
+
+# num1 = int(input("Enter the first number: "))  # Convert input to integer
+# num2 = int(input("Enter the second number: "))
+
+num1 = 10 
+num2 = 5
+
+# perform the difference and store the result
+difference = num1 - num2
+# Perform the addition and store the result
+sum = num1 + num2
+# perform the product and store the result
+product = num1 * num2
+
+print("The sum of", num1, "and", num2, "is", sum)
+print("The difference between", num1, "and", num2, "is", difference)
