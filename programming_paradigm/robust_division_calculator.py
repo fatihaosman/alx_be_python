8862388569f05684a1f95a84ca7e9913db12a7b5
fatihaosman <fatihaosman17@gmail.com>
2025-11-30 08:56:49 +0300

    just added robust deivision to handle zerodivision

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,6 +6,6 @@ def  safe_divide(numerator, denominator):
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
-        return "Error: Please enter numeric values only"
+        return "Error: Please enter numeric values only."
     finally:
         print("Division attempt completed.")
