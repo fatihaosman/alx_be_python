366726bb2957da2f29961501f8533ba0139d0254
fatihaosman <fatihaosman17@gmail.com>
2025-11-30 09:00:14 +0300

    just added robust deivision to handle zerodivision

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,7 +3,7 @@ def  safe_divide(numerator, denominator):
         num = float(numerator)
         denom = float(denominator)
         result = num / denom
-        return f"The result of the division is {result}"
+        return f"The result of the division is {result.f}"
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
