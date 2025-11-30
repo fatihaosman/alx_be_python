b9e3eef019dabce300e016dc52f50b3ee49198c0
fatihaosman <fatihaosman17@gmail.com>
2025-11-30 09:59:28 +0300

    just added tescase testing foor +, - / *

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,41 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Create calculator before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        """Test addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-5, -5), -10)
+
+    def test_subtract(self):
+        """Test subtraction method."""
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(3, 5), -2)
+        self.assertEqual(self.calc.subtract(-2, -2), 0)
+
+    def test_multiply(self):
+        """Test multiplication method."""
+        self.assertEqual(self.calc.multiply(4, 5), 20)
+        self.assertEqual(self.calc.multiply(-3, 3), -9)
+        self.assertEqual(self.calc.multiply(0, 10), 0)
+
+    def test_divide(self):
+        """Test division method."""
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(9, 3), 3)
+        self.assertEqual(self.calc.divide(-8, 2), -4)
+
+    def test_divide_by_zero(self):
+        """Test division by zero returns None."""
+        self.assertIsNone(self.calc.divide(5, 0))
+        self.assertIsNone(self.calc.divide(-10, 0))
+
+
+if __name__ == "__main__":
+    unittest.main()
