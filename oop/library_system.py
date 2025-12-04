c2122a08663fa47787aac1c1f319cacda22afbff
fatihaosman <fatihaosman17@gmail.com>
2025-12-04 16:53:28 +0300

    added library-system-implementing inheritance

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,15 +2,12 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-    def __str__(self):
-        return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size  # in MB
-    def __str__(self):
-        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"    
+
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
